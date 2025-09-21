d8b8ee6b8a592bd09500e758660e980fbb0cfca6
Ibrahim Tajudeen <donslice6@gmail.com>
2025-09-21 03:21:12 +0100

    All done III

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -1,10 +1,16 @@
 /* eslint-disable prettier/prettier */
 import { Module } from '@nestjs/common';
-import { AuthController } from './controllers/auth.controller';
+import { AuthController } from './auth.controller';
 import { DatabaseModule } from '../db/db.module';
+import { JwtModule } from '@nestjs/jwt';
+import { jwtConstants } from 'src/common/constants';
 
 @Module({
-  imports: [DatabaseModule],
+  imports: [DatabaseModule, JwtModule.register({
+      global: true,
+      secret: jwtConstants.secret,
+      signOptions: { expiresIn: '60s' },
+    })],
   controllers: [AuthController],
   providers: [],
 })
